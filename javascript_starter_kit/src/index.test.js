59c07f6cbedf62591b98c7858bbd47fa25335d54
Shane Foley <Shane.Foley@neudesic.com>
2017-06-27 12:49:22 -0700

    test

--- javascript_starter_kit/src/index.test.js
+++ javascript_starter_kit/src/index.test.js
@@ -15,7 +15,7 @@ describe('index.html', () => {
         jsdom.env(index, function(err, window){
             const h1 = window.document.getElementsByTagName('h1')[0];
             expect(h1.innerHTML).to.equal("Hello world!");
-            done();
+            done(); 
             window.close();
         });
     });
